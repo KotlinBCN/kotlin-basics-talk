60f505ab197ad4fc70ce826545752ae741e2fee6
Jose Luis  Franconetti Olmedo <joseluis@mobilejazz.com>
2017-07-13 17:47:52 +0200

    Added funtions

--- src/main/kotlin/com/kotlinbcn/kotlin/basics/functions/functions.kt
+++ src/main/kotlin/com/kotlinbcn/kotlin/basics/functions/functions.kt
@@ -1,3 +1,44 @@
 package com.kotlinbcn.kotlin.basics.functions
 
+fun functionTopLevelFile(): Boolean = true
+
+class Foo {
+
+    // member function
+    fun calculate(a: Int, b: Int): Int {
+
+        // local function
+        fun sum(a: Int, b: Int): Int = a + b
+
+        // local function
+        fun multiply(a: Int, b: Int): Int = a * b
+
+        return when {
+            a > b -> sum(a, b)
+            else -> multiply(a, b)
+        }
+    }
+
+    // function omitting the return type as it's Unit
+    fun function() = print("unit return type")
+}
+
+// Default parameters
+fun sum(a: Int = 1, b: Int = 1) = a + b
+fun multiply(a: Int = 1, b: Int) = a + b
+
+fun main(args: Array<String>) {
+    sum() // 2
+    sum(a = 2) // 3
+    multiply(b = 2) // 2
+}
+
+// Infix notations
+
+class Counter {
+    infix fun count (value: Array<Int>): Int = value.size
+}
+
+val counter = Counter()
+fun counter(values: Array<Int>): Int = counter count values
 
